1f9fd99273d4d5eaaea6106067a8a5ecde086fdc
Jean Emmanuel Kouadio <emmanuelkj5@gmail.com>
2026-03-15 20:56:27 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,49 +1,34 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-import datetime as dt
-import pandas as pd
-import random
-import smtplib
+import requests
+from twilio.rest import Client
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+weather_api_key = os.environ.get("WEATHER_API_KEY")
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
-# 2. Check if today matches a birthday in the birthdays.csv
-bd_file = pd.read_csv("birthdays.csv").to_dict("records")
-now = dt.datetime.now()
-for bd in bd_file:
-    bd_month = int(bd["month"])
-    bd_day = int(bd["day"])
-    bd_name = bd["name"]
-    bd_email = bd["email"]
-    # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-    if bd_month == now.month and bd_day == now.day:
-        # 4. Send the letter generated in step 3 to that person's email address.
-        # selecting and interacting with a random letter
-        # folder_path = 'letter_templates'
-        # file_pattern = '*.txt'
-        # letters = glob.glob(os.path.join(folder_path, file_pattern)) #get all .txt letters files
-        # random_letter = random.choice(letters) # select random letter file
+params = {
+    "lat":21.309919,
+    "lon":-157.858154,
+    "cnt":4,
+    "appid":weather_api_key
+}
 
-        #method 2 to get a random file
-        file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast?", params=params)
+response.raise_for_status()
 
-        # open and update the letter
-        with open(file_path, 'r') as f:
-            content = f.read() # get letter content as a list
-            content = content.replace('[NAME]',f'{bd_name}') #replace the name
-
-        # send the updated letter content to the person's email address
-        with smtplib.SMTP("smtp.gmail.com") as connection:
-            connection.starttls()
-            connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-            connection.sendmail(from_addr=MY_EMAIL,to_addrs=bd_email,
-                                msg=f"Subject:Happy Birthday!\n\n{content}")
+will_rain = False
+weather_data = response.json()
+data = weather_data["list"]
+for dt_list in data:
+    weather_id = dt_list["weather"][0]["id"]
+    if int(weather_id) < 700:
+        will_rain = True
+if will_rain:
+    print("Rain")
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_="whatsapp:+14155238886",
+        body="Bring an umbrella!",
+        to="whatsapp:+16812830298"
+    )
+    print(message.status)
